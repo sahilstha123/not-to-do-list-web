7faed1de1016de0a60fd5b56237079d37853e118
Sahil shrestha <sthasahil401@gmail.com>
2026-01-13 12:25:19 +0545

    Implement cors

--- server/server.js
+++ server/server.js
@@ -3,14 +3,15 @@ const morgan = require("morgan");
 const app = express();
 const taskRouter = require("./src/routers/taskRouter.js");
 const connectMongoDb = require("./src/config/dbConfig.js");
+const cors = require("cors")
 
 connectMongoDb();
 
 app.use(morgan("dev"));
 app.use(express.json()); 
+app.use(cors)
 
 app.use("/api/v1/tasks", taskRouter);
-
 
 app.use(require("./src/middlewares/errorHandler.js"));
 

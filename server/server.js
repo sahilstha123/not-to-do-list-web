e415f455f9285c507806b1ebf0425721f4deabd0
Sahil shrestha <sthasahil401@gmail.com>
2026-01-20 19:19:56 +0545

    modify server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,11 @@ connectMongoDb();
 app.use(morgan("dev"));
 app.use(express.json()); 
 app.use(cors())
+app.use((req, res, next) => {
+  console.log("Incoming:", req.method, req.url)
+  next()
+})
+
 
 app.use("/api/v1/tasks", taskRouter);
 
